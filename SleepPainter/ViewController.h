325e3a7bc671a823ddc64989874754ef109e6620
Shanshan ZHAO <chezzhaoss@gmail.com>
2015-02-24 23:16:55 +0100

    init commit project

---
+++ SleepPainter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SleepPainter
+//
+//  Created by Shanshan ZHAO on 24/02/15.
+//  Copyright (c) 2015 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SleepPainter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SleepPainter
+//
+//  Created by Shanshan ZHAO on 24/02/15.
+//  Copyright (c) 2015 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
