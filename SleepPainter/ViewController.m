b02534f5a31682d76ab4edd948f8c918a221970c
Shanshan ZHAO <chezzhaoss@gmail.com>
2015-02-25 00:03:35 +0100

    Add clock label and 2 background images in asset

--- SleepPainter/ViewController.m
+++ SleepPainter/ViewController.m
@@ -8,20 +8,72 @@
 
 #import "ViewController.h"
 
+#define CHANGE_SKY_INTERVAL 10
+
 @interface ViewController ()
+{
+    int dummyToggle; // to test sky image changing
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *clockLabel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+
+#pragma mark - view lifecycle
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    dummyToggle = 0;
+    [self updateClock];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - update clock and background image
+- (void)updateClock
+{
+    NSDateFormatter *clockFormat = [[NSDateFormatter alloc] init];
+    [clockFormat setDateFormat:@"h mm ss a"];
+    self.clockLabel.text = [clockFormat stringFromDate:[NSDate date]];
+    
+    // Test background image changing
+    [clockFormat setDateFormat:@"s"];
+    int seconds = [[clockFormat stringFromDate:[NSDate date]] intValue];
+    if (seconds % CHANGE_SKY_INTERVAL == 0)
+    {
+        [self updateBackgroundImage];
+    }
+    //
+    [self performSelector:@selector(updateClock) withObject:self afterDelay:1.0f];
+}
+
+- (void)updateBackgroundImage
+{
+    NSLog(@"Update background image");
+    
+    // Add animation
+    CATransition *animation = [CATransition animation];
+    animation.duration = 1.0f;
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    animation.type = kCATransitionFade;
+    animation.delegate = self;
+    [self.view.layer addAnimation:animation forKey:nil];
+    
+    // Do dummy action, changing image every ten seconds
+    switch (dummyToggle) {
+        case 0:
+            [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"SP_backgroundlight.png"]]];
+            break;
+        case 1:
+            [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"SP_backgroundDark.png"]]];
+            break;
+        default:
+            break;
+    }
+    dummyToggle = 1 - dummyToggle;
+    
 }
 
 @end
